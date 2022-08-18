ce529c713846f3f4c8fa2130471a20bcb7c98f97
Aditya Mandiyal <mandiyaladitya@gmail.com>
2022-08-18 22:58:37 +0530

    completed

--- ForEach/ForEach.java
+++ ForEach/ForEach.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ForEach{  
-    public static void main(String[] args) {  
-        List<String> nameList = new ArrayList<String>();  
-        nameList.add("Arjun");  
-        nameList.add("Aditya");  
-        nameList.add("Sahil");  
-        nameList.add("Deepak");  
-        nameList.forEach(System.out::println);  
-    }  
+	 public static void main(String[] args) {  
+	        List<String> namesList = new ArrayList<String>();  
+	        namesList.add("Aditya");  
+	        namesList.add("Arjun");  
+	        namesList.add("Ronit");  
+	        namesList.add("Deepak");  
+	        namesList.forEach(names -> System.out.println(names));  
+	          
+	    }  
 }
